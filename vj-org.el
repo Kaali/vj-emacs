a532013f5eb6df1ada921f06b980b721af93dee2
Väinö Järvelä <vaino.jarvela@fudeco.com>
2011-01-02 14:04:32 +0200

    Added missing org-mode bindings

--- vj-org.el
+++ vj-org.el
@@ -3,6 +3,10 @@
 (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
 (add-to-list 'auto-mode-alist '("\\.org.txt$" . org-mode))
 
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cb" 'org-iswitchb)
+
 ;; Remove org-mode meta-cursor bindings as it's used by windmove
 (add-hook 'org-mode-hook
           '(lambda ()
