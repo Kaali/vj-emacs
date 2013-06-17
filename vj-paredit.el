7e92032541f66e90d5a59dd53408c25d2bab1e64
Väinö Järvelä <vaino.jarvela@spinfy.com>
2013-06-17 20:24:51 +0300

    Fixed paredit conflict with dwim

--- vj-paredit.el
+++ vj-paredit.el
@@ -5,3 +5,8 @@
 (add-hook 'emacs-lisp-mode-hook       (lambda () (paredit-mode +1)))
 (add-hook 'lisp-mode-hook             (lambda () (paredit-mode +1)))
 (add-hook 'lisp-interaction-mode-hook (lambda () (paredit-mode +1)))
+
+;;; Disable conflicting shortcuts (not in vj-keyboard as this is just hiding,
+;;; not defining)
+(define-key paredit-mode-map (kbd "M-<up>") nil)
+(define-key paredit-mode-map (kbd "M-<down>") nil)
