98972a211f964a4028c17da7bdc348f260f02781
Väinö Järvelä <vaino.jarvela@spinfy.com>
2013-06-17 20:24:33 +0300

    Added ac-nrepl to clojure

--- vj-clojure.el
+++ vj-clojure.el
@@ -3,8 +3,28 @@
 (defun turn-on-paredit () (paredit-mode 1))
 (add-hook 'clojure-mode-hook 'turn-on-paredit)
 
+; nrepl
 (add-hook 'nrepl-interaction-mode-hook
    'nrepl-turn-on-eldoc-mode)
 (setq nrepl-popup-stacktraces-in-repl t)
 (add-to-list 'same-window-buffer-names "*nrepl*")
 (add-hook 'nrepl-mode-hook 'paredit-mode)
+
+; auto-complete
+(require 'ac-nrepl)
+(add-hook 'nrepl-mode-hook 'ac-nrepl-setup)
+(add-hook 'nrepl-interaction-mode-hook 'ac-nrepl-setup)
+(eval-after-load "auto-complete"
+    '(add-to-list 'ac-modes 'nrepl-mode))
+
+(defun set-auto-complete-as-completion-at-point-function ()
+   (setq completion-at-point-functions '(auto-complete)))
+(add-hook 'auto-complete-mode-hook 'set-auto-complete-as-completion-at-point-function)
+
+(add-hook 'nrepl-mode-hook 'set-auto-complete-as-completion-at-point-function)
+(add-hook 'nrepl-interaction-mode-hook 'set-auto-complete-as-completion-at-point-function)
+
+; Replace nrepl doc lookup with ac-nrepl
+; TODO: Not working yet
+(define-key nrepl-interaction-mode-map (kbd "C-c C-d") 'ac-nrepl-popup-doc)
+
